bc8ac4837781c3172f28d6822adf328830b5de6c
AndreNIH <31828626+AndreNIH@users.noreply.github.com>
2022-04-06 01:10:02 -0700

    big update

--- app/src/main/java/DataManager/Vehicle.java
+++ app/src/main/java/DataManager/Vehicle.java
@@ -1,20 +1,57 @@
 package DataManager;
+
 import DataManager.Pool.*;
 
-public class Vehicle extends Component{
-    //TODO: Add fields and get/set methods
-    
-    public Vehicle(Model model) throws ComponentNotBoundToPool{
-        if(model == null);
-        if(!ModelPool.get().componentIsRegisteredAtPool(model)){
-            throw new ComponentNotBoundToPool("The 'model' object passed to bindToModel does not exist inside ModelPool");
+public class Vehicle extends Component {
+    // TODO: Add fields and get/set methods
+    private String VIN = new String();
+    private String licensePlate = new String();
+    private String color = new String();
+    private Double mileage = 0.0;
+
+    public Vehicle(Model model) throws ComponentNotBoundToPool {
+        if (model == null)
+            ;
+        if (!ModelPool.get().componentIsRegisteredAtPool(model)) {
+            throw new ComponentNotBoundToPool(
+                    "The 'model' object passed to bindToModel does not exist inside ModelPool");
         }
         model.addChild(this);
     }
-    
-    public Model getModel(){
-        return (Model)getParent();
+
+    public Model getModel() {
+        return (Model) getParent();
+    };
+
+    public String getVIN() {
+        return this.VIN;
     }
-    
-    
+
+    public void setVIN(String numVIN) {
+        this.VIN = numVIN;
+    }
+
+    public String getLicensePlate() {
+        return this.licensePlate;
+    }
+
+    public void setlicensePlate(String newLicensePlate) {
+        this.licensePlate = newLicensePlate;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    public Double getMileage() {
+        return this.mileage;
+    }
+
+    public void setMileage(Double newMillage) {
+        this.mileage = newMillage;
+    }
 }
